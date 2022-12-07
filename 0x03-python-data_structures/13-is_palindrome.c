10c6142c369472ccdfdc1f33dc729c21dd5253d7
john526 <tiahadoua@gmail.com>
2022-12-07 03:39:37 +0000

    file

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * is_palindrome - checks if palindrome
+ * @head: head of node
+ * Return: 0 if not, 1 if is
  */
-int main(void)
+int is_palindrome(listint_t **head)
 {
-	listint_t *head;
+	unsigned int len = 1;
+	listint_t *temp;
 
+	if (head == NULL|| *head == NULL)
+		return (1);
+
+	temp = *head;
+	while (temp) /* get len of list */
+	{
+		temp = temp->next;
+		len++;
+	}
 	return (0);
 }
